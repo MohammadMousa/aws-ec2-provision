f1eff4b7a710219f7d545e18b9498bda29f7212e
MohammadMousa <mohammad.mousa@kartngo.co>
2026-08-11 05:57:15 +0300

    Add disposable EC2 test instance Terraform template

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd*/ubuntu-*-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "test_web" {
+  name        = "test-web-sg"
+  description = "Disposable test instance - SSH, HTTP, HTTPS"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "test_web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = "alpha"
+  vpc_security_group_ids = [aws_security_group.test_web.id]
+
+  tags = {
+    Name    = "ansible-clean-test"
+    Purpose = "disposable"
+  }
+}
+
+output "test_instance_public_ip" {
+  value = aws_instance.test_web.public_ip
+}
